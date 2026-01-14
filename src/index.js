63248e0601e4d3438e03da43b84039b1b67b8c70
= <=>
2026-01-14 21:56:04 +0530

    modified index.js

--- src/index.js
+++ src/index.js
@@ -1,31 +1,20 @@
-import mongoose from "mongoose"
-import {DB_NAME} from "./constants.js"
 import connectDB from './db/index.js'
 import dotenv from "dotenv"
-
+import {app} from "./app.js"
 dotenv.config({path:'./.env'})
+
+
 connectDB()
-
-/*
-import express from "express"
-const app = express()
-;(async ()=>{
-    try {
-        await mongoose.connect(`${process.env.MONGODB_URL}/${DB_NAME}`)
-        app.on("error", (error)=>{//if error in express app in listening
-            console.log("ERROR: ", error)
-            throw error
-        })  
-        app.listen(process.env.PORT||3000, ()=>{
-            console.log(`app listening on ${process.env.PORT}`)
-        })
-    }
-    catch(error) {
-        console.error("ERROR: ", error)
+.then( () => {
+    app.on("error", (error) => {
+        console.log("ERROR:" , error)
         throw error
-    }               
-})()
+    })
 
-
----not a good practice
-*/
+    app.listen(process.env.PORT||3000, () => {
+        console.log(`server is running at port ${process.env.PORT||3000}`)
+    })
+})
+.catch( (error) => {
+    console.log("MONDO db connection failed", error)
+})
