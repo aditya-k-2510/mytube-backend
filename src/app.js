1ac7f2dacc3001a661cdf696b876a370aa09266f
Aditya Khetrapal <khetrapaladitya2510@gmail.com>
2026-01-30 20:38:09 +0530

    feat(app.js): add healthcheck and dashboard routes to the application

--- src/app.js
+++ src/app.js
@@ -21,11 +21,17 @@ import userRouter from "./routes/user.routes.js";
 import videoRouter from "./routes/video.routes.js";
 import tweetRouter from "./routes/tweet.routes.js"
 import subscriptionRouter from "./routes/subscription.routes.js"
+import healthCheckRouter from "./routes/healthcheck.routes.js"
+import dashboardRouter from "./routes/dashboard.routes.js"
+
 
 //routes declaration
 app.use("/api/v1/users", userRouter)
 app.use("/api/v1/videos", videoRouter)
 app.use("/api/v1/tweets", tweetRouter)
 app.use("/api/v1/subscription", subscriptionRouter)
+app.use("/api/v1/healthcheck", healthCheckRouter)
+app.use("/api/v1/dashboard", dashboardRouter)
+
 
 export { app }
