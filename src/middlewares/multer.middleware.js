2406124664fbcd2bf9e565b3cbab47cc21609142
Aditya Khetrapal <khetrapaladitya2510@gmail.com>
2026-03-03 19:17:47 +0530

    fix(middleware): remove unnecessary import and clean up multer configuration

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,4 +1,5 @@
 import multer from "multer";
+import fs from "fs"
 
 const storage = multer.diskStorage({
    destination: function (req, file, cb) {
@@ -10,5 +11,5 @@ const storage = multer.diskStorage({
 });
 
 export const upload = multer({
-   storage,
+   storage
 });
