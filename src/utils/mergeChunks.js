b244f9e19ff433aeb5078b8272706aee85c10a21
Aditya Khetrapal <khetrapaladitya2510@gmail.com>
2026-03-03 19:18:00 +0530

    feat(utils): add mergeChunks utility for combining video upload chunks

---
+++ src/utils/mergeChunks.js
@@ -0,0 +1,24 @@
+import fs from "fs";
+
+export const mergeChunks = async (fileId, fileName, totalChunks) => {
+
+   const chunkDir = `./public/temp/chunkUploads/${fileId}`;
+   const finalPath = `${chunkDir}/${fileName}`;
+
+   const writeStream = fs.createWriteStream(finalPath);
+
+   for (let i = 0; i < totalChunks; i++) {
+      const chunkPath = `${chunkDir}/${i}`;
+      const data = fs.readFileSync(chunkPath);
+      if (!writeStream.write(data)) {
+         await new Promise(resolve =>
+            writeStream.once("drain", resolve)
+         );
+      }
+      fs.unlinkSync(chunkPath);
+   }
+   await new Promise(resolve =>
+      writeStream.end(resolve)
+   );
+   return finalPath;
+};
