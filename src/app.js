554396fefc6553643c0b18ee0fcf47004384a1a4
Aditya Khetrapal <khetrapaladitya2510@gmail.com>
2026-03-14 18:56:52 +0530

    fix(app): add express.text middleware for handling text payloads

--- src/app.js
+++ src/app.js
@@ -12,6 +12,7 @@ app.use(
 );
 
 app.use(express.json({ limit: "16kb" }));
+app.use(express.text());
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public")); //for logos, image, etc.
 app.use(cookieParser());
