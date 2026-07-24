ad92d65238154e77664c163034b8a04a052a872b
Aditya Khetrapal <khetrapaladitya2510@gmail.com>
2026-07-24 21:43:42 +0530

    fix: unique file name

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -6,7 +6,8 @@ const storage = multer.diskStorage({
       cb(null, "./public/temp");
    },
    filename: function (req, file, cb) {
-      cb(null, file.originalname);
+      const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+      cb(null, uniqueSuffix + "-" + file.originalname);
    },
 });
 
